1818864489b66d6162024f9e01297bd580a2cb92
Plouis53 <Plouis53@gmail.com>
2023-05-23 17:26:09 -0400

    updates

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const { PORT = 3001 } = process.env;
 const app = express();
+mongoose.connect("mongodb://localhost:27017/wtwr_db");
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
