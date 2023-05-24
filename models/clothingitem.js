af1bfe12f459a3e851de544c8b443b9307ebc730
Plouis53 <Plouis53@gmail.com>
2023-05-23 21:02:30 -0400

    add clothing schemas

---
+++ models/clothingitem.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const clothingSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+});
+
+module.exports = mongoose.model("item", clothingSchema);
