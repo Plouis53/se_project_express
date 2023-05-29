fb5f43b38849908fd6e68247a198d69e58e3eaca
Plouis53 <Plouis53@gmail.com>
2023-05-29 17:15:26 -0400

    update errors

--- utils/errors.js
+++ utils/errors.js
@@ -1,14 +1,9 @@
-const errorCode400 = 400;
-const errorCode404 = 404;
-const errorCode500 = 500;
+const ERROR_400 = 400;
+const ERROR_404 = 404;
+const ERROR_500 = 500;
 
 module.exports = {
-  errorCode400,
-  errorCode404,
-  errorCode500,
+  ERROR_400,
+  ERROR_404,
+  ERROR_500,
 };
-
-
-const ERROR_CODE = 400;
-
-if(err.name === 'SomeErrorName') return res.status(ERROR_CODE).send({ message: "Appropriate error message" })
