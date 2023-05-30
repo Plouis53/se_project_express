53f5180ed268b8698e39430798aecf41b92448ea
Plouis53 <Plouis53@gmail.com>
2023-05-30 15:42:44 -0400

    fixed syntax errors

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 const router = require("express").Router();
-const User=require("./user")
+const User = require("./user");
 
-const clothingItem = require("../models/clothingItem");
+const clothingItem = require("./clothingitem");
 
 router.use("/items", clothingItem);
-router.use("/users," User);
+router.use("/users", User);
 
-router.use((_req, res) => {
-  res.status(500).send({ message: "Routrer not found" });
+router.use((req, res) => {
+  res
+    .status(404)
+    .send({
+      message:
+        "There is no clothing item with the requested id, or the request was sent to a non-existent address",
+    });
 });
-
 
 module.exports = router;
