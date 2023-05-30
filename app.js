52a7b131aa02dfe2f564ade91ca6cdb7a3187611
Plouis53 <Plouis53@gmail.com>
2023-05-29 23:38:01 -0400

    Last updates

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const mongoose = require("mongoose");
 
 const { PORT = 3001 } = process.env;
 const app = express();
-mongoose.connect(
-  "mongodb://localhost:27017/wtwr_db",
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db"),
   (r) => {
-    console.log("connected to DB");
-  },
-  (e) => console.log("DB Error", e)
-);
+    console.log("connected to DB", r);
+  };
+
+  app.use((req, res, next) => {
+    req.user = {
+      _id: '5d8b8592978f8bd833ca8133'// paste the _id of the test user created in the previous step
+    };
+    next();
+  });
+
 
 const routes = require("./routes");
 
--- routes/clothingitem.js
+++ routes/clothingitem.js
@@ -9,21 +9,16 @@ const {
 } = require("../controllers/clothingItem");
 
 //CRUD
-
 //CREATE // POST /items - Create a new clothing item
 router.post("/items", createItem);
-
 // READ // GET /items - Get all clothing items
 router.get("/items", getItems);
-
 // UPDATE // PUT /items/:itemId/likes - Like an item
 router.put("/items/:itemId/likes", likeItem);
-
 // DELETE // DELETE /items/:itemId - Delete clothing item by ID
 router.delete("/items/:itemId", deleteItem);
 // DELETE /items/:itemId/likes - Unlike an item
 router.delete("/items/:itemId/likes", disLikeItem);
-
 // Handle non-existent resource
 router.use((req, res) => {
   res.status(404).json({ message: "Requested resource not found" });
