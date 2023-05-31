b53b95452daff54ec785e7e31f0fd267ca176cf8
Plouis53 <Plouis53@gmail.com>
2023-05-31 17:20:09 -0400

    new updates 5

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (r) => {
   console.log("connected to DB", r);
 });
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: "5d8b8592978f8bd833ca8133", // paste the _id of the test user created in the previous step
-  };
-  next();
-});
+// app.use((req, res, next) => {
+//   req.user = {
+//     _id: "5d8b8592978f8bd833ca8133", // paste the _id of the test user created in the previous step
+//   };
+//   next();
+// });
 
 const routes = require("./routes");
 
