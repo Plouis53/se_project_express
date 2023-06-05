939884cb969afdeffe37a7debea0c684c9e46e82
Plouis53 <Plouis53@gmail.com>
2023-06-05 11:09:22 -0400

    fix error

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 
 const { getUser, getUsers, createUser } = require("../controllers/user");
 
-//CRUDE// POST / - Create a new user
+// CRUDE
+// POST / - Create a new user
 router.post("/", createUser);
-
 // GET / - Get all users
 router.get("/", getUsers);
 // GET /- Get user by ID
