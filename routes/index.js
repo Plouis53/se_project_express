e0d560fd3f0a3dfe754437e60da3ba28a1f1278d
Plouis53 <Plouis53@gmail.com>
2023-06-05 11:40:15 -0400

    imported error code 404

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 const router = require("express").Router();
 const User = require("./user");
-
 const clothingItem = require("./clothingitem");
+const { ERROR_404 } = require("../utils/errors");
 
 router.use("/items", clothingItem);
 router.use("/users", User);
 
 router.use((req, res) => {
-  res.status(400).send({
+  res.status(ERROR_404).send({
     message:
       "There is NO API with the requested path, or the request was sent to a non-existent address",
   });
 });
 
 module.exports = router;
+
+
