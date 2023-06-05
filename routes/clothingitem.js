e162974b3dc2d16fc2f700087b4ddc56c2753bdf
Plouis53 <Plouis53@gmail.com>
2023-06-05 11:49:07 -0400

    removed additional items

--- routes/clothingitem.js
+++ routes/clothingitem.js
@@ -9,20 +9,27 @@ const {
   disLikeItem,
 } = require("../controllers/clothingItem");
 
-// CRUD//CREATE //- Create a new clothing item
+// CREATE - Create a new clothing item
 router.post("/", createItem);
-// READ //- Get all clothing items
-router.get("/", getItems);
-// UPDATE // - Like an item
-router.put("/items/:itemId/likes", likeItem);
-router.put("/:itemId", updateItem);
-// DELETE //- Delete clothing item by ID & dislike an item
-router.delete("/items/:itemId", deleteItem);
-router.delete("/items/:itemId/likes", disLikeItem);
 
-// // Handle non-existent resource
-// router.use((req, res) => {
-//   res.status(404).json({ message: "Requested resource not found" });
-// });
+// READ - Get all clothing items
+router.get("/", getItems);
+
+// UPDATE - Like an item
+router.put("/:itemId/likes", likeItem);
+
+// UPDATE - Update an item
+router.put("/:itemId", updateItem);
+
+// DELETE - Delete a clothing item by ID
+router.delete("/:itemId", deleteItem);
+
+// DELETE - Unlike an item
+router.delete("/:itemId/likes", disLikeItem);
 
 module.exports = router;
+
+// // // Handle non-existent resource
+// // router.use((req, res) => {
+// //   res.status(404).json({ message: "Requested resource not found" });
+// // });
