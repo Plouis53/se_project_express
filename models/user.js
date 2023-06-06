5f191907aea847545d7cefba0a46547cc5f29382
Plouis53 <Plouis53@gmail.com>
2023-06-06 13:21:42 -0400

    inserted email & password

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const user = new mongoose.Schema({
     required: [true, "Name is required"],
     minlength: [2, "Name must be at least 2 characters long"],
     maxlength: [30, "Name cannot exceed 30 characters"],
+    default: "Elise Bouer", // Set default value for name field
   },
   avatar: {
     type: String,
@@ -15,6 +16,21 @@ const user = new mongoose.Schema({
       validator: (value) => validator.isURL(value),
       message: "You must enter a valid URL",
     },
+    default:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Elise.png", // Set default value for avatar field
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    validate: {
+      validator: (value) => validator.isEmail(value),
+      message: "Invalid email address",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
   },
 });
 
