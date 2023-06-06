b0c4661520948567ac5b8ce38b8d327d23147b98
Plouis53 <Plouis53@gmail.com>
2023-06-06 18:31:44 -0400

    update file

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,12 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
+const { ERROR_401 } = require("../utils/errors");
 
 const authMiddleware = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res.status(401).json({ error: "Unauthorized" });
+    return res.status(ERROR_401).json({ error: "Unauthorized" });
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -15,7 +16,7 @@ const authMiddleware = (req, res, next) => {
     req.user = payload;
     next();
   } catch (error) {
-    return res.status(401).json({ error: "Unauthorized" });
+    return res.status(ERROR_401).json({ error: "Unauthorized" });
   }
 };
 
