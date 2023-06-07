0e4f0cdbb000b76228e0aa4b78285cba37322c7c
Plouis53 <Plouis53@gmail.com>
2023-06-07 12:13:51 -0400

    cleaned up code

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,27 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
-const { ERROR_401, handleError } = require("../utils/errors");
+const { errorStatusCodes, handleError } = require("../utils/errors");
 
 module.exports.authorization = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res.status(ERROR_401).json({ error: "Unauthorized" });
-  }
-
-  const token = authorization.replace("Bearer ", "");
-
   try {
-    const payload = jwt.verify(token, JWT_SECRET);
-    req.user = payload
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization.startsWith("Bearer ")) {
+      res.status(errorStatusCodes.unauthorized).send({ error: "Unauthorized" });
+      return;
+    }
+
+    const token = authorization.replace("Bearer ", "");
+    let payload;
+
+    try {
+      payload = jwt.verify(token, JWT_SECRET);
+    } catch (error) {
+      res.status(errorStatusCodes.unauthorized).send({ error: "Unauthorized" });
+      return;
+    }
+
+    req.user = payload;
     next();
   } catch (error) {
     handleError(error, res);
