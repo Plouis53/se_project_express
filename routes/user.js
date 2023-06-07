34e44161452cf4a19774bd382ea7e2bbe1f49d2c
Plouis53 <Plouis53@gmail.com>
2023-06-06 22:07:51 -0400

    update

--- routes/user.js
+++ routes/user.js
@@ -1,29 +1,19 @@
 const router = require("express").Router();
-const { getUser, getUsers, createUser } = require("../controllers/user");
-
-// Remove the following routes:
-// router.post("/", createUser);
-// router.get("/", getUsers);
-// router.get("/:id", getUser);
-
-// Add the updated routes:
-router.post("/", createUser); // POST / - Create a new user
-router.get("/", getUsers); // GET / - Get all users
-router.get("/:userId", getUser); // GET /:userId - Get user by ID
-
-module.exports = router;
-
-
-// const router = require("express").Router();
+const auth = require("../middlewares/auth");
 
 // const { getUser, getUsers, createUser } = require("../controllers/user");
 
-// // CRUDE
-// // POST / - Create a new user
-// router.post("/", createUser);
-// // GET / - Get all users
+const { getCurrentUser, updateCurrentUser } = require("../controllers/user");
+// CRUDE
+// POST / - Create a new user
+//router.post("/", createUser);
+// GET / - Get all users
 // router.get("/", getUsers);
-// // GET /- Get user by ID
-// router.get("/:userId", getUser);
+// GET /- Get user by ID
+//router.get("/:userId", getUser);
+router.get("/me", auth, getCurrentUser);
 
-// module.exports = router;
+// UPDATE
+router.patch("/me", auth, updateCurrentUser);
+
+module.exports = router;
--- utils/errors.js
+++ utils/errors.js
@@ -1,11 +1,27 @@
 const ERROR_400 = 400;
 const ERROR_401 = 401;
+const ERROR_403 = 403;
 const ERROR_404 = 404;
+const ERROR_409 = 409;
 const ERROR_500 = 500;
+const ERROR_1100 = 1100;
 
 module.exports = {
   ERROR_400,
   ERROR_401,
+  ERROR_403,
   ERROR_404,
+  ERROR_409,
   ERROR_500,
+  ERROR_1100,
 };
+
+// const ERROR_CODES = {
+//   BadRequest: 400,
+//   Unauthorized: 401,
+//   Forbidden: 403,
+//   NotFound: 404,
+//   AlreadyExistsError: 409,
+//   DefaultError: 500,
+//   MongoError: 11000,
+// };
