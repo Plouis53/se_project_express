58becc8237933977e31524d27360fdb766d0d0dd
Plouis53 <Plouis53@gmail.com>
2023-06-06 22:41:54 -0400

    revise file

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 const { PORT = 3001 } = process.env;
 const app = express();
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (r) => {
-  console.log("connected to DB", r);
-});
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: "5d8b8592978f8bd833ca8133", // paste the _id of the test user created in the previous step
-  };
-  next();
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (error) => {
+  if (error) {
+    console.log("Failed to connect to the database:", error);
+  } else {
+    console.log("Connected to the database");
+  }
 });
 
 const routes = require("./routes");
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
-  console.log("is this working?");
+  console.log(`App listening at port ${PORT}`);
 });
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+
+// const { PORT = 3001 } = process.env;
+// const app = express();
+// mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (r) => {
+//   console.log("connected to DB", r);
+// });
+
+// app.use((req, res, next) => {
+//   req.user = {
+//     _id: "5d8b8592978f8bd833ca8133", // paste the _id of the test user created in the previous step
+//   };
+//   next();
+// });
+
+// const routes = require("./routes");
+
+// app.use(express.json());
+// app.use(routes);
+
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on ${PORT}`);
+//   console.log("is this working?");
+// });
