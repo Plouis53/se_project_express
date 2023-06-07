017e8cf928e009e738b0ff4b73eff672f37ecfaf
Plouis53 <Plouis53@gmail.com>
2023-06-07 11:39:09 -0400

    update errors

--- utils/errors.js
+++ utils/errors.js
@@ -1,46 +1,87 @@
-const ERROR_400 = 400;
-const ERROR_401 = 401;
-const ERROR_403 = 403;
-const ERROR_404 = 404;
-const ERROR_409 = 409;
-const ERROR_500 = 500;
-const ERROR_1100 = 1100;
-
-module.exports = {
-  ERROR_400,
-  ERROR_401,
-  ERROR_403,
-  ERROR_404,
-  ERROR_409,
-  ERROR_500,
-  ERROR_1100,
-  handleItemError,
-  handleError,
+const errorStatusCodes = {
+  badRequest: 400,
+  unauthorized: 401,
+  forbidden: 403,
+  notFound: 404,
+  conflict: 409,
+  internalServerError: 500,
+  mongoError: 11000,
 };
 
-const handleItemError = () => {
+const handleOnFailError = () => {
   const error = new Error("No item found");
-  error.status (ERROR_404);
+  error.statusCode = errorStatusCodes.notFound;
   throw error;
 };
 
-const handleError = (err, res) => {
-  if (err.name === "ValidationError" || err.name === "CastError") {
-    res.status(ERROR_400).send({ message: "Bad Request, Invalid input" });
-  } else if (err.message === "Incorrect email or password") {
+const handleError = (error, res) => {
+  if (error.name === "ValidationError" || error.name === "CastError") {
     res
-      .status(ERROR_401)
+      .status(errorStatusCodes.badRequest)
+      .send({ message: "Bad Request, Invalid input" });
+  } else if (error.message === "Incorrect email or password") {
+    res
+      .status(errorStatusCodes.unauthorized)
       .send({ message: "You are not authorized to do this" });
-  } else if (err.statusCode === ERROR_404) {
-    res.status(ERROR_404).send({ message: "Item not found" });
-  } else if (err.code === ERROR_1100) {
-    res.status(ERROR_409).send({
-      message: "Email address is already being used, please try another email.",
-    });
+  } else if (error.statusCode === errorStatusCodes.notFound) {
+    res.status(errorStatusCodes.notFound).send({ message: "Item not found" });
+  } else if (error.code === errorStatusCodes.mongoError) {
+    res
+      .status(errorStatusCodes.conflict)
+      .send({
+        message:
+          "Email address is already being used, please try another email.",
+      });
   } else {
-    res.status(ERROR_500).send({ message: "Something went wrong" });
+    res
+      .status(errorStatusCodes.internalServerError)
+      .send({ message: "Something went wrong" });
   }
 };
+
+// const ERROR_400 = 400;
+// const ERROR_401 = 401;
+// const ERROR_403 = 403;
+// const ERROR_404 = 404;
+// const ERROR_409 = 409;
+// const ERROR_500 = 500;
+// const ERROR_1100 = 1100;
+
+// module.exports = {
+//   ERROR_400,
+//   ERROR_401,
+//   ERROR_403,
+//   ERROR_404,
+//   ERROR_409,
+//   ERROR_500,
+//   ERROR_1100,
+//   handleItemError,
+//   handleError,
+// };
+
+// const handleItemError = () => {
+//   const error = new Error("No item found");
+//   error.status (ERROR_404);
+//   throw error;
+// };
+
+// const handleError = (err, res) => {
+//   if (err.name === "ValidationError" || err.name === "CastError") {
+//     res.status(ERROR_400).send({ message: "Bad Request, Invalid input" });
+//   } else if (err.message === "Incorrect email or password") {
+//     res
+//       .status(ERROR_401)
+//       .send({ message: "You are not authorized to do this" });
+//   } else if (err.statusCode === ERROR_404) {
+//     res.status(ERROR_404).send({ message: "Item not found" });
+//   } else if (err.code === ERROR_1100) {
+//     res.status(ERROR_409).send({
+//       message: "Email address is already being used, please try another email.",
+//     });
+//   } else {
+//     res.status(ERROR_500).send({ message: "Something went wrong" });
+//   }
+// };
 
 // const ERROR_CODES = {
 //   BadRequest: 400,
