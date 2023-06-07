d4b5f7fb8a60232fb458e0d26b8fee1730415bde
Plouis53 <Plouis53@gmail.com>
2023-06-07 13:42:59 -0400

    included new errorStatusCodes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const User = require("./user");
 const clothingItem = require("./clothingitem");
-const { ERROR_404 } = require("../utils/errors");
+const { errorStatusCodes } = require("../utils/errors");
 
 const { login, createUser } = require("../controllers/user");
 
@@ -12,27 +12,10 @@ router.post("/signin", login);
 router.post("/signup", createUser);
 
 router.use((req, res) => {
-  res.status(ERROR_404).send({
+  res.status(errorStatusCodes.notFound).send({
     message:
       "There is NO API with the requested path, or the request was sent to a non-existent address",
   });
 });
 
 module.exports = router;
-
-// const router = require("express").Router();
-// const User = require("./user");
-// const clothingItem = require("./clothingitem");
-// const { ERROR_404 } = require("../utils/errors");
-
-// router.use("/items", clothingItem);
-// router.use("/users", User);
-
-// router.use((req, res) => {
-//   res.status(ERROR_404).send({
-//     message:
-//       "There is NO API with the requested path, or the request was sent to a non-existent address",
-//   });
-// });
-
-// module.exports = router;
