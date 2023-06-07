41f41b69b3c0453442b9373306f51f8b2501395b
Plouis53 <Plouis53@gmail.com>
2023-06-07 13:40:23 -0400

    added auth

--- routes/clothingitem.js
+++ routes/clothingitem.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const auth = require("../middlewares/auth");
 
 const {
   getItems,
@@ -10,21 +11,21 @@ const {
 } = require("../controllers/clothingItem");
 
 // CREATE - Create a new clothing item
-router.post("/", createItem);
+router.post("/", auth, createItem);
 
 // READ - Get all clothing items
 router.get("/", getItems);
 
 // UPDATE - Like an item
-router.put("/:itemId/likes", likeItem);
+router.put("/:itemId/likes", auth, likeItem);
 
 // UPDATE - Update an item
-router.put("/:itemId", updateItem);
+router.put("/:itemId", auth, updateItem);
 
 // DELETE - Delete a clothing item by ID
-router.delete("/:itemId", deleteItem);
+router.delete("/:itemId", auth, deleteItem);
 
 // DELETE - Unlike an item
-router.delete("/:itemId/likes", disLikeItem);
+router.delete("/:itemId/likes", auth, disLikeItem);
 
 module.exports = router;
