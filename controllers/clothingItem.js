19363d41cbe3536e92d1b2e9e7cb96f4681c7cc8
Plouis53 <Plouis53@gmail.com>
2023-06-08 09:58:16 -0400

    delete conditions

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -30,8 +30,10 @@ const updateItem = (req, res) => {
   const { imageUrl } = req.body;
 
   ClothingItem.findOneAndUpdate({ _id: itemId }, { $set: { imageUrl } })
-    .orFail(() => handleOnFailError())
-    .then((item) => res.status(200).send({ data: item }))
+    .orFail(handleOnFailError)
+    .then((item) => {
+      res.send({ data: item });
+    })
     .catch((err) => {
       handleErrorResponse(err, res);
     });
@@ -41,18 +43,18 @@ const deleteItem = (req, res) => {
   const { itemId } = req.params;
 
   ClothingItem.findById(itemId)
-    .orFail(() => handleOnFailError())
+    .orFail(handleOnFailError)
     .then((item) => {
       if (String(item.owner) !== req.user._id) {
         return res
           .status(errorStatusCodes.forbidden)
           .send({ message: "You are not authorized to delete this item" });
       }
+
       return item.deleteOne().then(() => {
         res.send({ message: "Item deleted" });
       });
     })
-
     .catch((err) => {
       if (err.statusCode === errorStatusCodes.notFound) {
         res
@@ -78,10 +80,10 @@ const likeItem = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true }
   )
-    .orFail(() => handleOnFailError())
-    .then(() =>
-      res.status(200).send({ message: "Item has successfully been liked" })
-    )
+    .orFail(handleOnFailError)
+    .then(() => {
+      res.send({ message: "Item has successfully been liked" });
+    })
     .catch((err) => {
       handleErrorResponse(err, res);
     });
@@ -95,8 +97,10 @@ const disLikeItem = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true }
   )
-    .orFail(() => handleOnFailError())
-    .then((item) => res.status(200).send({ data: item }))
+    .orFail(handleOnFailError)
+    .then((item) => {
+      res.send({ data: item });
+    })
     .catch((err) => {
       handleErrorResponse(err, res);
     });
@@ -110,58 +114,3 @@ module.exports = {
   likeItem,
   disLikeItem,
 };
-
-// function handleRegularItemError(req, res, err) {
-//   console.error(err);
-//   if (err.name === "ValidationError" || err.name === "AssertionError") {
-//     return res.status(ERROR_400).send({
-//       message:
-//         "Invalid data passed to the methods for creating an item or updating an item, or invalid ID passed to the params.",
-//     });
-//   }
-//   if (err.name === "CastError") {
-//     return res.status(ERROR_400).send({
-//       message:
-//         "Invalid ID passed to the params. There is no clothing item with the requested id, or the request was sent to a non-existent address.",
-//     });
-//   }
-//   return res
-//     .status(ERROR_500)
-//     .send({ message: "An error has occurred on the server" });
-// }
-
-// function handleFindByIdItemError(req, res, err) {
-//   if (
-//     err.name === "CastError" ||
-//     err.name === "ValidationError" ||
-//     err.name === "AssertionError"
-//   ) {
-//     return res.status(ERROR_400).send({
-//       message:
-//         "Invalid data passed to the methods for creating an item or updating an item, or invalid ID passed to the params.",
-//     });
-//   }
-//   if (err.name === "DocumentNotFoundError") {
-//     return res.status(ERROR_404).send({
-//       message:
-//         "There is no clothing item with the requested id, or the request was sent to a non-existent address.",
-//     });
-//   }
-//   return res
-//     .status(ERROR_500)
-//     .send({ message: "An error has occurred on the server" });
-// }
-
-// const deleteItem = (req, res) => {
-//   const { itemId } = req.params;
-//   console.log("HERE", req.params.itemId);
-
-//   ClothingItem.findByIdAndDelete(itemId)
-//     .orFail()
-//     .then(() =>
-//       res.send({ message: `The item has been successfully deleted.` })
-//     )
-//     .catch((err) => {
-//       handleFindByIdItemError(req, res, err);
-//     });
-// };
