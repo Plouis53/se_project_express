8e0058460f4823c71af8ac050a34581a32a59e9d
Plouis53 <Plouis53@gmail.com>
2023-07-10 17:25:30 -0400

    added errors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const errorHandler = require("./middlewares/error-handler");
+const { errors } = require("celebrate");
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -14,7 +15,14 @@ const routes = require("./routes");
 
 app.use(express.json());
 app.use(cors());
+
+// our routes
 app.use(routes);
+
+// celebrate error handler
+app.use(errors());
+
+// our centralized handler
 app.use(errorHandler);
 
 app.listen(PORT, () => {
