d0534d195a2f227d5ae8527d668acd14bb2646e3
Plouis53 <Plouis53@gmail.com>
2023-07-27 16:31:25 -0400

    comment out JWT_SECRET

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
-const { JWT_SECRET = "my-secret-key" } = process.env;
+// const { JWT_SECRET = "my-secret-key" } = process.env;
 
-module.exports = {
-  JWT_SECRET,
-};
+// module.exports = {
+//   JWT_SECRET,
+// };
