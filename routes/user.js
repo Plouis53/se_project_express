9dbb022e7123074177b8fd830780450b2128a56c
Plouis53 <Plouis53@gmail.com>
2023-07-28 13:24:37 -0400

    added validateCreatedUserInfo

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,12 @@
 const router = require("express").Router();
-const {authorization} = require("../middlewares/auth");
+const { authorization } = require("../middlewares/auth");
+const { validateCreatedUserInfo } = require("../middlewares/validation");
 
-const {
-  // getUser,
-  // getUsers,
-  // createUser,
-  getCurrentUser,
-  updateCurrentUser,
-} = require("../controllers/user");
+const { getCurrentUser, updateCurrentUser } = require("../controllers/user");
 
-// CRUDE
-
-// POST / - Create a new user
-// router.post("/", createUser);
-
-// GET / - Get all users
-// router.get("/", getUsers);
-// GET /- Get user by ID
-// router.get("/:userId", getUser);
 router.get("/me", authorization, getCurrentUser);
 
 // UPDATE
-router.patch("/me", authorization, updateCurrentUser);
+router.patch("/me", authorization, validateCreatedUserInfo, updateCurrentUser);
 
 module.exports = router;
