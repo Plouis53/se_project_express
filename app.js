b7e3d7c172263dae409aed8dcdda8f18c2b20881
Plouis53 <Plouis53@gmail.com>
2024-04-27 16:00:57 -0400

    changed port back to 3001

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { errors } = require("celebrate"); // Import celebrate before error-handle
 const errorHandler = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
-const { PORT = 3002 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (error) => {
